dd96c0e2740483a7a574d3643290e9fbc16daf8c
Josh <admin@elmhoe.co.uk>
2018-01-28 17:47:34 +0000

    Update 28.01.2018 - Progress again on perfecting commandline version of a media player.

--- src/uk/co/ElmHoe/Utilities/HTTPUtility.java
+++ src/uk/co/ElmHoe/Utilities/HTTPUtility.java
@@ -3,6 +3,7 @@ package uk.co.ElmHoe.Utilities;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -11,11 +12,10 @@ public class HTTPUtility {
 	private static String USER_AGENT = "ElmHoeABLogs/20.0";
 
 	
-	public static void sendPost(String urlParameters) throws Exception {
+	public static void sendPost(String url, String urlParameters) throws Exception {
 
 		try{
-			//String url = "http://server.elmhoe.co.uk/randomFileForLogging.php";
-			String url = "http://server.elmhoe.co.uk/login.php";
+			//String url = "http://server.elmhoe.co.uk/login.php";
 			URL obj = new URL(url);
 			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 	
@@ -50,4 +50,41 @@ public class HTTPUtility {
 */		
 		}
 	}
+	
+	
+	
+	public static String sendGet(String url) throws Exception {
+
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nHTTPUtility > Sending 'GET' request to URL : " + url);
+		System.out.println("HTTPUtility > Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println("HTTPUtility > " + response.toString() + "\n");
+		
+		
+		return response.toString();
+
+	}
+
 }
